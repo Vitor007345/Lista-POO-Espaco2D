2c0a4dbf97c3db3f657b37e16ef5db8e1133de19
Vitor007345 <vitordinelli2007@hotmail.com>
2026-04-22 18:39:20 -0300

    Criação do projeto

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	private static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
